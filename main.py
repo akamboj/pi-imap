251320e801b22a1881c069bbaed164193f3c18c0
Anoop Kamboj <akamboj2000@gmail.com>
2018-07-04 15:00:15 -0700

    Can process emails to TurnOnPC now

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ import sys
 import imaplib
 import email
 import email.header
+import subprocess
+
 import getpass
+import ConfigParser
+
 
 # https://gist.github.com/robulouski/7441883
 
 OK_RV = 'OK'
 COMMAND_WORD = 'COMMAND'
+COMMAND_LIST = ['TurnOnPC']
+COMMANDS = {
+    'TurnOnPC' : {
+        'script' : 'TurnOnPC.sh'
+    }
+}
 
-EMAIL_ACCOUNT = "rpi.akamboj@gmail.com"
-
-CONFIG_TRUSTED_ADDRESSES = ['']
+CONFIG_TRUSTED_ADDRESSES = ['rpi.akamboj@gmail.com']
+CONFIG_EMAIL_ACCOUNT = 'rpi.akamboj@gmail.com'
+CONFIG_EMAIL_PASSWORD = 'Byibgifvutwac67'
 
 def main():
+    read_config()
+
     mailbox = imaplib.IMAP4_SSL('imap.gmail.com')
 
-    rv, data = mailbox.login(EMAIL_ACCOUNT, "Arag0rn13")
+    rv, data = mailbox.login(CONFIG_EMAIL_ACCOUNT, CONFIG_EMAIL_PASSWORD)
     rv, list = mailbox.list()
     print list
 
@@ -28,13 +40,16 @@ def main():
 def log(str):
     print str
 
+def read_config():
+    pass
+
 def process_mailbox(mailbox):
     
     rv, results = mailbox.search(None, "ALL")
 
     for num in results[0].split():
         rv, data = mailbox.fetch(num, '(RFC822)')
-        if rv != 'OK':
+        if rv != OK_RV:
             print "ERROR getting message", num
             return
 
@@ -54,45 +69,48 @@ def process_mailbox(mailbox):
 
         print 'Message %s: %s' % (num, subject)
 
-        if validate_message(senderEmail, subject) == True:
+        validationSucceeded, msg = validate_message(senderEmail, subject)
+        if validationSucceeded == True:
             
-            command = get_command_from_subject(subject)
-            process_command(command)
+            process_command(msg)
             pass
         else:
             log('Invalid email!\nSender: %s\nSubject: %s' % (senderEmail, subject))
             
 
         
+        # mailbox.store(num, '+FLAGS', '\\Deleted')
+
+    # Not sure if we need to actually expunge
+    # mailbox.expunge()
 
 
-    pass
-
+# Return a tuple. 1st param, True if validation succeeded, if True, the 2nd param is the Command, if not it's why the validation failed
 def validate_message(sender, subject):
     if sender not in CONFIG_TRUSTED_ADDRESSES:
-        return False
+        return False, ''
 
     # Split the command word and the actual command from the subject
     # We assume subjects take the form "'COMMAND_WORD'-'COMMAND'"
     splitSubject = subject.split('-')
 
-    # Due to the above our subjects should have len of 2 after splitting
+    # Due to the above, our subjects should have len of 2 after splitting
     if len(splitSubject) != 2:
-        return False
+        return False, ''
     if splitSubject[0] != COMMAND_WORD:
-        return False
+        return False, ''
 
-    COMMAND_LIST = ['']
-    if splitSubject[1] not in COMMAND_LIST:
-        return False
+    requestedCommand = splitSubject[1]
+    if requestedCommand not in COMMANDS:
+        return FALSE, ''
 
-    return True, splitSubject[1]
+    return True, requestedCommand
 
-def get_command_from_subject():
-    return ''
 
 def process_command(command):
-    pass
+    scriptName = COMMANDS[command]['script']
+    subprocess.call([scriptName])
+
 
 if __name__ == "__main__":
     main()
